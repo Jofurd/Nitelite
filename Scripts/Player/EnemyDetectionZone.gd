97ea34f8a52605c068299e3287ff9f27f4133fc6
Tyler <tylerkunze@gmail.com>
2021-10-19 19:16:24 -0500

    Scene Transitions added/started on Temple area
    scene changer prefabs can be place into a scene and set to whichever scene to change
    to in the inspector. the transition is unfished and just cuts abruptly.
    
    music has also been disabled in preparation to find a better system that works
    more smoothly with scene changes.

--- Scripts/Player/EnemyDetectionZone.gd
+++ Scripts/Player/EnemyDetectionZone.gd
@@ -8,11 +8,10 @@ func is_colliding():
 	return areas.size() > 0
 
 
-func _process(_delta):
-	var areas = get_overlapping_areas()
-	if is_colliding():
-		if areas.size() > 0:
-			if actionTrack.volume_db <= -1:
-					actionTrack.volume_db += 1
-	elif actionTrack.volume_db >= -50:
-		actionTrack.volume_db -= 0.12
+#func _process(_delta):
+#	if is_colliding():
+#		if areas.size() > 0:
+#			if actionTrack.volume_db <= -1:
+#					actionTrack.volume_db += 1
+#	elif actionTrack.volume_db >= -50:
+#		actionTrack.volume_db -= 0.12
